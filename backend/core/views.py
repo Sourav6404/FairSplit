af568f29af32c958b0e2c7e1e362f5d12d59d51a
Sourav Kuriakose <souravkuriakose6404@gmail.com>
2026-06-14 22:33:47 +0530

    Add DRF API endpoints for groups members expenses and settlements

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import (
+    Group,
+    Member,
+    Expense,
+    Settlement
+)
+
+from .serializers import (
+    GroupSerializer,
+    MemberSerializer,
+    ExpenseSerializer,
+    SettlementSerializer
+)
+class GroupViewSet(viewsets.ModelViewSet):
+
+    queryset = Group.objects.all()
+
+    serializer_class = GroupSerializer
+class MemberViewSet(viewsets.ModelViewSet):
+
+    queryset = Member.objects.all()
+
+    serializer_class = MemberSerializer
+class ExpenseViewSet(viewsets.ModelViewSet):
+
+    queryset = Expense.objects.all()
+
+    serializer_class = ExpenseSerializer
+class SettlementViewSet(viewsets.ModelViewSet):
+
+    queryset = Settlement.objects.all()
+
+    serializer_class = SettlementSerializer
